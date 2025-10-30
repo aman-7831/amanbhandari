58d3ef860fd314e9c99f7d4034855317feae2106
aman-7831 <amanbhandari188@gmail.com>
2025-10-31 03:51:04 +0545

    Initial commit and setup deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   // 🚨 FINAL VALUE: Base set to your repository name
-  base: '/amanbhandari.com.np/', 
+  base: '/amanbhandari/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
